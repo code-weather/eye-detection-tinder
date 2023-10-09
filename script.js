0eb0748bef543d33ac0ad9563632896198710981
Jameson Wang <jamesonw10@gmail.com>
2023-10-09 19:54:48 -0400

    add css file

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ webgazer
 function getNewImage(next = false) {
   const img = document.createElement('img');
   img.src = 'https://picsum.photos/1000?' + Math.random();
-
+  if (next) img.classList.add('next');
   document.body.append(img);
+  return img;
 }
